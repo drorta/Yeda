1bf3a570d6702f4414c90f3b54befb26e0d375a9
Dror Talmi <dror.gg.talmi@gmail.com>
2024-09-24 14:53:29 +0300

    doxxx

--- src/L4.java
+++ src/L4.java
@@ -1,6 +1,28 @@
 public class L4 {
 
     public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>(); //פעולה בונה מחסנית ריקה
+
+        st.push(5);
+        System.out.println(st.toString());
+
+        st.push(7);
+        System.out.println(st.toString());
+
+        st.push(8);
+        System.out.println(st.toString());
+
+        st.push(3);
+        System.out.println(st.toString());
+
+        int num = st.pop(); //מוציא איבר עליון רק אם לא ריק
+        System.out.println(st.toString());
+
+        int moreNum = st.pop();
+        System.out.println(st.toString());
+
+        st.push(0);
+        System.out.println(st.toString());
 
     }
 
