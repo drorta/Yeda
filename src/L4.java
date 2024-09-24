542ad2e18f048c2f65f3e3305e3f5a26b6f0d051
Dror Talmi <dror.gg.talmi@gmail.com>
2024-09-24 14:42:25 +0300

    first commit

---
+++ src/L4.java
@@ -0,0 +1,7 @@
+public class L4 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/Node.java
@@ -0,0 +1,28 @@
+public class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    public Node (T value) {
+        this.value = value;
+        this.next = null;
+    }
+    public Node (T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+    public T getValue() {
+        return value;
+    }
+    public Node<T> getNext() {
+        return next;
+    }
+    public void setValue(T value) {
+        this.value = value;
+    }
+    public void setNext (Node<T> next) {
+        this.next = next;
+    }
+    public String toString() {
+        return ""+value;
+    }
+}
