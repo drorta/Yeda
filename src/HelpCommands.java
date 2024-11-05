ec44fa3d5641fbc0186bc32569d966aff536c943
drorta <125767200+drorta@users.noreply.github.com>
2024-11-05 14:52:55 +0200

    Added HelpCommands

---
+++ src/HelpCommands.java
@@ -0,0 +1,117 @@
+public class HelpCommands {
+
+    public static Stack<Integer> copySt(Stack<Integer> st) {
+        Stack<Integer> temp = new Stack<Integer>();
+        Stack<Integer> copy = new Stack<Integer>();
+        while (!st.isEmpty()) {
+            temp.push(st.pop());
+        }
+        while (!temp.isEmpty()) {
+            st.push(temp.top());
+            copy.push(temp.pop());
+        }
+        return copy;
+    }
+
+    public static boolean hasChar(Stack<Character> st, char c){
+        Stack<Character> temp = new Stack<Character>();
+        while (!st.isEmpty()) {
+            if (st.top() == c) {
+                return true;
+            }
+            temp.push(st.pop());
+        }
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+        return false;
+    }
+
+    public static int numOfCharInSt(Stack<Character> st, char c){
+        Stack<Character> temp = new Stack<Character>();
+        int count = 0;
+        while (!st.isEmpty()) {
+            if (st.top() == c) {
+                count++;
+            }
+            temp.push(st.pop());
+        }
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+        return count;
+    }
+
+    public static void removeChar(Stack<Character> st, char c) {
+        Stack<Character> temp = new Stack<Character>();
+        while (!st.isEmpty()) {
+            char popped = st.pop();
+            if (popped ==  c) {
+                c = Character.MAX_VALUE;
+            } else {
+                temp.push(popped);
+            }
+        }
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+    }
+
+
+    public static int stLen(Stack<Integer> st) {
+        int len = 0;
+        Stack<Integer> temp = new Stack<Integer>();
+        while (!st.isEmpty()) {
+            len++;
+            temp.push(st.pop());
+        }
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+        return len;
+    }
+
+    public static boolean isSame(Stack<Integer> st1, Stack<Integer> st2) {
+        Stack<Integer> temp1 = copySt(st1);
+        Stack<Integer> temp2 = copySt(st2);
+        if (stLen(st1) != stLen(st2)) {
+            return false;
+        }
+        while (!temp1.isEmpty()) {
+            int a = temp1.pop();
+            int b = temp2.pop();
+            if (a != b) return false;
+        }
+        return true;
+    }
+
+    public static int numOfAppearances(Queue<Integer> queue, int x) {
+        int counter = 0;
+        Queue<Integer> temp = new Queue<Integer>();
+        while (!queue.isEmpty()) {
+            if (queue.head() == x) {
+                counter++;
+            }
+            temp.insert(queue.remove());
+        }
+        while (!temp.isEmpty()) {
+            queue.insert(temp.remove());
+        }
+        return counter;
+    }
+
+    public static void removeAllX(Queue<Integer> queue, int x) {
+        Queue<Integer> temp = new Queue<Integer>();
+        while (!queue.isEmpty()) {
+            if (queue.head() == x) {
+                queue.remove();
+            } else {
+                temp.insert(queue.remove());
+            }
+        }
+        while (!temp.isEmpty()) {
+            queue.insert(temp.remove());
+        }
+    }
+
+}
