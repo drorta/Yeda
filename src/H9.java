d9892b184fd21f00527a6d820229db229718c608
Dror Talmi <dror.gg.talmi@gmail.com>
2024-11-25 20:45:58 +0200

    breakkkkkkkkkkkk

--- src/H9.java
+++ src/H9.java
@@ -14,6 +14,7 @@ public class H9 extends HelpCommands {
         while (node != null) {
             if (node.getValue() == value) {
                 has = true;
+                break;
             }
             node = node.getNext();
         }
