e89de653482cf673ff29c670610e115c151bf153
Dror Talmi <dror.gg.talmi@gmail.com>
2024-11-25 20:44:31 +0200

    done

--- src/H9.java
+++ src/H9.java
@@ -1,5 +1,45 @@
 public class H9 extends HelpCommands {
 
+    public static void changeOdd(Node<Integer> node){
+        while (node != null) {
+            if (node.getValue() % 2 == 1) {
+                node.setValue(node.getValue()*2);
+            }
+            node = node.getNext();
+        }
+    }
+
+    public static boolean hasValue(Node<Integer> node, int value) {
+        boolean has = false;
+        while (node != null) {
+            if (node.getValue() == value) {
+                has = true;
+            }
+            node = node.getNext();
+        }
+        return has;
+    }
+
+    public static int countVal(Node<Integer> node, int value) {
+        int count = 0;
+        while (node != null) {
+            if (node.getValue() == value) {
+                count++;
+            }
+            node = node.getNext();
+        }
+        return count;
+    }
+
+    public static int findMin(Node<Integer> node) {
+        int min = node.getValue();
+        while (node != null) {
+            min = Math.min(min, node.getNext().getValue());
+            node = node.getNext();
+        }
+        return min;
+    }
+
     public static boolean ex3pg89(Node<Integer> node) {
         int prev = Integer.MIN_VALUE;
         while (node != null) {
