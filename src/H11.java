f28d3121e0f3e52245830f3ecd70ff4850517937
Dror Talmi <dror.gg.talmi@gmail.com>
2024-12-06 23:21:57 +0200

    b

--- src/H11.java
+++ src/H11.java
@@ -4,6 +4,28 @@ public class H11 extends HelpCommands {
         return removeValue(node, value);
     }
 
+    public static Node<Integer> ex13(Node<Integer> node){
+        Node<Integer> temp = node;
+        if (lenNodes(temp) % 2 == 0) {
+            for (int i = 0; i < lenNodes(temp) / 2 - 1; i++) {
+                temp = temp.getNext();
+            }
+            temp.setNext(
+                    Math.max(temp.getNext().getValue(), temp.getNext().getNext().getValue()) == temp.getNext().getValue() ?
+                            temp.getNext() : temp.getNext().getNext()
+            );
+            return node;
+        } else {
+            node = node.getNext();
+            temp = temp.getNext();
+            for (int i = 0; i < lenNodes(node); i++) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+            return node;
+        }
+    }
+
     public static void main(String[] args) {
         Node<Integer> node = new Node<>(4);
         node.setNext(buildSortNode(10));
