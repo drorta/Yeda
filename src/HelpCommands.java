fe832633e9a213620d49bbf5cdd809d9cc235db4
drorta <125767200+drorta@users.noreply.github.com>
2025-01-07 14:45:43 +0200

    update

--- src/HelpCommands.java
+++ src/HelpCommands.java
@@ -284,6 +284,57 @@ public class HelpCommands {
     }
 
     /**
+     * @param queue
+     * @return the length of the queue
+     */
+    public static int lengthQueue(Queue<Integer> queue) {
+        int len = 0;
+        Queue<Integer> temp = new Queue<>();
+        while (!queue.isEmpty()) {
+            temp.insert(queue.remove());
+            len++;
+        }
+        while (!temp.isEmpty()) {
+            queue.insert(temp.remove());
+        }
+        return len;
+    }
+
+    /**
+     * Returns a flipped form of the given queue while not destroying the original queue
+     *
+     * @param queue
+     * @return the flipped queue
+     */
+    public static Queue<Integer> flipQueue(Queue<Integer> queue) {
+        Queue<Integer> temp = new Queue<>();
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> flip = new Queue<>();
+        while (!queue.isEmpty()) {
+            temp.insert(queue.head());
+            stack.push(queue.remove());
+        }
+        while (!temp.isEmpty()) {
+            queue.insert(temp.remove());
+            flip.insert(stack.pop());
+        }
+        return flip;
+    }
+
+    /**
+     * flips the given queue recursively
+     *
+     * @param q1
+     */
+    public static void flipQueueRec(Queue<Double> q1) {
+        if (!q1.isEmpty()) {
+            double x = q1.remove();
+            flipQueueRec(q1);
+            q1.insert(x);
+        }
+    }
+
+    /**
      * Prints a node and all nodes linked to it
      *
      * @param node the first node
