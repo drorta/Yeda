b5f853d9fe353df12c150a474de4adc6894c584b
drorta <125767200+drorta@users.noreply.github.com>
2025-01-19 22:38:24 +0200

    maybe

--- src/Report.java
+++ src/Report.java
@@ -1,2 +1,7 @@
 public record Report(Date dateGiven, int carID, int offenceID, int payment) {
+
+    Report(){
+        this(new Date(-1, Month.DECEMBER, -1), -1, -1, -1);
+    }
+
 }
--- src/ReportDataBase.java
+++ src/ReportDataBase.java
@@ -1,2 +1,26 @@
 public class ReportDataBase {
+
+    private Node<Report> reports;
+
+    public ReportDataBase(){
+        this.reports = new Node<>(new Report());
+    }
+
+    public void moveReportToLawyer(Date date){
+        /*
+        לא כתבו אם לכתוב את זה או לא... אני מניח שלא צריך
+         */
+    }
+
+    public int carPaymentTotal(int carID){
+        Node<Report> temp = reports;
+        int sum = 0;
+        while (temp != null) {
+            if (temp.getValue().carID() == carID) {
+                sum += temp.getValue().payment();
+            }
+        }
+        return sum;
+    }
+
 }
