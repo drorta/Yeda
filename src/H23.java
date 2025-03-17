dae6db35b84633f91076e6712d7e20a44ac70304
drorta <125767200+drorta@users.noreply.github.com>
2025-03-17 18:14:08 +0200

    skibibibibibiibib

--- src/H23.java
+++ src/H23.java
@@ -1,4 +1,16 @@
+import classes.*;
+
 public class H23 extends Main {
+
+    public int legalCities(CameraInfo[] cameraInfos){
+        int legalCities = 0;
+        for (CameraInfo cameraInfo : cameraInfos){
+            if (cameraInfo.allGood()) {
+                legalCities++;
+            }
+        }
+        return legalCities;
+    }
 
     public static void main(String[] args) {
 
